0066e10592b1d2dd28fa78b62c842a8c90dfb60d
nathan <52280764@qq.com>
2017-06-30 14:46:48 +0800

    1

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-define('MASKED_DOMAIN', 'http://www.xxxx.com');
+define('MASKED_DOMAIN', 'http://www.tzecc.com/');
 define('PROXY_SUBFOLDER', '');
 define('FOLLOW_LOCATION', FALSE);
--- proxy.php
+++ proxy.php
@@ -1,30 +1,42 @@
 <?php
-
-/**
- * Set extra multiple options for cURL
- * Could be used to define CURLOPT_SSL_VERIFYPEER & CURLOPT_SSL_VERIFYHOST for HTTPS
- * Also to overwrite any other options without changing the code
- * See http://php.net/manual/en/function.curl-setopt-array.php
- */
-$curl_options = array(
-    // CURLOPT_SSL_VERIFYPEER => false,
-    // CURLOPT_SSL_VERIFYHOST => 2,
-);
-
 require "config.php";
 
-$reqUrl = $_SERVER['REQUEST_URI'];
+function cn_urlencode($url){
+    $pregstr = "/[\x{4e00}-\x{9fa5}]+/u";//UTF-8中文正则
+    if(preg_match_all($pregstr,$url,$matchArray)){//匹配中文，返回数组
+        foreach($matchArray[0] as $key=>$val){
+            $url=str_replace($val, urlencode($val), $url);//将转译替换中文
+        }
+        if(strpos($url,' ')){//若存在空格
+            $url=str_replace(' ','%20',$url);
+        }
+    }
+    return $url;
+}
+
+$uri = $_GET["dst"] ;
+//$uri =  $_SERVER['REQUEST_URI'];
+//$uri = "Media/Default/home/视觉天职-1.png";
+
 if(PROXY_SUBFOLDER != '')
     $uri = str_replace(PROXY_SUBFOLDER, '',$_SERVER['REQUEST_URI']);
+$url = MASKED_DOMAIN . $uri;
 
-$reqUrl = MASKED_DOMAIN . $reqUrl;
+$url = cn_urlencode($url);
 
-/* * * STOP EDITING HERE UNLESS YOU KNOW WHAT YOU ARE DOING * * */
 
-// identify request headers
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+
 $request_headers = array( );
 foreach ($_SERVER as $key => $value) {
-    if (strpos($key, 'HTTP_') === 0  ||  strpos($key, 'CONTENT_') === 0) {
+    if (!strpos($key, 'Connection') ) {
         $headername = str_replace('_', ' ', str_replace('HTTP_', '', $key));
         $headername = str_replace(' ', '-', ucwords(strtolower($headername)));
         if (!in_array($headername, array( 'Host', 'X-Proxy-Url' ))) {
@@ -33,69 +45,38 @@ foreach ($_SERVER as $key => $value) {
     }
 }
 
-// identify request method, url and params
-$request_method = $_SERVER['REQUEST_METHOD'];
-if ('GET' == $request_method) {
-    $request_params = $_GET;
-} elseif ('POST' == $request_method) {
-    $request_params = $_POST;
-    if (empty($request_params)) {
-        $data = file_get_contents('php://input');
-        if (!empty($data)) {
-            $request_params = $data;
-        }
-    }
-} elseif ('PUT' == $request_method || 'DELETE' == $request_method) {
-    $request_params = file_get_contents('php://input');
-} else {
-    $request_params = null;
-}
-
-$request_url = $reqUrl;
+//curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);   // (re-)send headers
 
 
-$p_request_url = parse_url($request_url);
+ $cookies = "";
+foreach($_COOKIE as $k=>$v)
+    $cookies .= $k.'='.$v.';';
+curl_setopt($ch, CURLOPT_COOKIE, '"'.$cookies. '"');
 
-
-// append query string for GET requests
-if ($request_method == 'GET' && count($request_params) > 0 && (!array_key_exists('query', $p_request_url) || empty($p_request_url['query']))) {
-    $request_url .= '?' . http_build_query($request_params);
-}
-
-// let the request begin
-$ch = curl_init($request_url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);   // (re-)send headers
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     // return response
-curl_setopt($ch, CURLOPT_HEADER, true);       // enabled response headers
-// add data for POST, PUT or DELETE requests
-if ('POST' == $request_method) {
-    $post_data = is_array($request_params) ? http_build_query($request_params) : $request_params;
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,  $post_data);
-} elseif ('PUT' == $request_method || 'DELETE' == $request_method) {
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $request_method);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $request_params);
-}
-
-// Set multiple options for curl according to configuration
-if (is_array($curl_options) && 0 <= count($curl_options)) {
-    curl_setopt_array($ch, $curl_options);
-}
 
 // retrieve response (headers and content)
 $response = curl_exec($ch);
 curl_close($ch);
 
+
 // split response to header and content
 list($response_headers, $response_content) = preg_split('/(\r\n){2}/', $response, 2);
 
+header_remove();
 // (re-)send the headers
 $response_headers = preg_split('/(\r\n){1}/', $response_headers);
 foreach ($response_headers as $key => $response_header) {
-    if (!preg_match('/^(Transfer-Encoding):/', $response_header)) {
-        header($response_header, false);
+    if (!preg_match('/^(Transfer-Encoding):/', $response_header) && !preg_match('/^(Content-Length):/', $response_header) ) {
+        header($response_header, true);
     }
 }
+function remove_utf8_bom($text)
+{
+    $bom = pack('H*','EFBBBF');
+    $text = preg_replace("/^$bom/", '', $text);
+    return $text;
+}
 
-// finally, output the content
-print($response_content);
+echo $response_content;
+
+
